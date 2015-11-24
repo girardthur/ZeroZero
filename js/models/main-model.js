7434dc72c9ba4a791cad97d08c88b83b274edf64
Arthur Girard <ArthurGirard@MacBook-Pro.local>
2015-11-24 04:48:48 +0100

    Main model updated
    Attributes updated

---
+++ js/models/main-model.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+(function () {
+    'use strict';
+
+    app.MainModel = Backbone.Model.extend({
+
+        defaults: {
+            id: 1000,
+            description: 'main description',
+            sound: false,
+            money: 0,
+            multiplier: 1,
+            incomePerClick: 3,
+            incomePerSecond: 2
+            //restaurants: RestauCollec,
+            //player: PlayerCollec,
+        }
+
+    });
+})();
